aab5b89a01f22da74ca7f433f6b78a375d4e2d39
Windu Nursetyadi <windudr@gmail.com>
2022-04-12 23:40:11 +0700

    push ar

---
+++ Assets/Scripts/ExitAR.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExitAR : MonoBehaviour
+{
+    public Canvas start;
+    public Button exit;
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
